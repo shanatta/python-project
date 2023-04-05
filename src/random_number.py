16a1e453d1ae7f70cd14899cf39c3c57069707f6
Shanatta <114990082+shanatta@users.noreply.github.com>
2023-04-05 19:22:47 +0700

    Create random_number.py

---
+++ src/random_number.py
@@ -0,0 +1,12 @@
+#Random number
+#-menampilkan angka acak antara 0 sampai 36
+ 
+#-mengimpor modul random
+import random
+ 
+#-menampilkan angka acak
+print("Angka", random.randint(0,36)) 
+print("Angka", random.randint(0,36)) 
+print("Angka", random.randint(0,36)) 
+print("Angka", random.randint(0,36)) 
+print("Angka", random.randint(0,36)) 
