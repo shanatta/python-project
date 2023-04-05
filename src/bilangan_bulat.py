29c71ddf78baa11adf64b3c59a56d83479717e9a
Shanatta <114990082+shanatta@users.noreply.github.com>
2023-04-05 19:12:16 +0700

    Create bilangan_bulat.py

---
+++ src/bilangan_bulat.py
@@ -0,0 +1,16 @@
+#Menentukan bilangan positif, negatif, atau nol
+
+#-menginput angka
+angka = float(input("Tulis Sebuah Angka: "))
+ 
+#-menampilkan kondisi angka positif
+if angka > 0:
+   print("Angka Positif")
+ 
+#-menampilkan kondisi angka nol   
+elif angka == 0:
+   print("Angka Nol")
+ 
+#-menampilkan kondisi angka negatif
+else:
+   print("Angka Negatif") 
